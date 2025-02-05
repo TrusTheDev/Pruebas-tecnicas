34fe7bdc476b356075f2c3bd356806950a5bbaca
Trusty <123142834+IvanViegas@users.noreply.github.com>
2025-02-04 23:59:56 -0300

    caracteres infiltrados
    /*
     * Crea una función que reciba dos cadenas de texto casi iguales,
     * a excepción de uno o varios caracteres.
     * La función debe encontrarlos y retornarlos en formato lista/array.
     * - Ambas cadenas de texto deben ser iguales en longitud.
     * - Las cadenas de texto son iguales elemento a elemento.
     * - No se pueden utilizar operaciones propias del lenguaje
     *   que lo resuelvan directamente.
     *
     * Ejemplos:
     * - Me llamo mouredev / Me llemo mouredov -> ["e", "o"]
     * - Me llamo.Brais Moure / Me llamo brais moure -> [" ", "b", "m"]
     */

---
+++ caracterInfiltrado.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+/*
+ * Crea una función que reciba dos cadenas de texto casi iguales,
+ * a excepción de uno o varios caracteres.
+ * La función debe encontrarlos y retornarlos en formato lista/array.
+ * - Ambas cadenas de texto deben ser iguales en longitud.
+ * - Las cadenas de texto son iguales elemento a elemento.
+ * - No se pueden utilizar operaciones propias del lenguaje
+ *   que lo resuelvan directamente.
+ *
+ * Ejemplos:
+ * - Me llamo mouredev / Me llemo mouredov -> ["e", "o"]
+ * - Me llamo.Brais Moure / Me llamo brais moure -> [" ", "b", "m"]
+ */
+
+public class caracterInfiltrado {
+    public static void main(String[] args) {
+        String palabra1 = "hola mundo";
+        String palabra2 = "hola mudial";
+        List<String> palabras = caracteresInfiltrados(palabra1, palabra2);
+
+        for(int i=0; i<palabras.size(); i++){
+            System.out.println(palabras.get(i));
+        }
+    }
+
+    static List<String> caracteresInfiltrados(String palabra1, String palabra2){
+        List<String> infiltrados = null;
+        for(int i=0; i<palabra1.length(); i++){
+            if (palabra1.charAt(i) != palabra2.charAt(i)){
+                infiltrados.add(String.valueOf(palabra1.charAt(i)));
+            }
+        }
+        return infiltrados;
+    }
+}
